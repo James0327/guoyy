176274de6ceb8e62595c644ca2fc28eba4be086b
james.guo <james.guo@ly.com>
2021-12-24 20:58:05 +0800

    --

--- test/src/test/java/com/jw/j/MyTest.java
+++ test/src/test/java/com/jw/j/MyTest.java
@@ -2,9 +2,6 @@ package com.jw.j;
 
 import org.junit.jupiter.api.Test;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-
 /**
  * Description: guoyy
  * com.jw.j.MyTest
@@ -19,6 +16,9 @@ public class MyTest {
 
     @Test
     public void test() {
+        String ret0 = String.format("%+.2f元", 3 / 100D);
+        System.out.println("ret0: " + ret0);
+
         String ret1 = String.format("%+d%%", -1);
         System.out.println(ret1);
 
